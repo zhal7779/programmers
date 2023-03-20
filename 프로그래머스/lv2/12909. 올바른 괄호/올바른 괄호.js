366f0da326ce888409df39f913ffb056d81d0e90
zhal7779 <79049046+zhal7779@users.noreply.github.com>
2023-03-21 01:05:41 +0900

    [level 2] Title: 올바른 괄호, Time: 3.02 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,24 @@
+function solution(s){
+    let answer = true;
+    let bracket = 0;
+    if (s[0] === ')'){
+        return false
+    }
+    
+    for(let i = 0; i< s.length; i++){
+       if(s[i] === '('){
+           bracket++;
+       } else {
+           bracket--;
+       }   
+    
+        if(bracket < 0){
+            answer = false;
+            break;
+        } 
+    }
+    if(bracket !== 0){
+        return false
+    }
+    return answer;
+}
