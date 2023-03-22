c35b79f0faa8b49c8abb9a6933ff471deb4eda55
zhal7779 <79049046+zhal7779@users.noreply.github.com>
2023-03-22 15:27:41 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,17 @@
+
+function solution(n, words) {
+  let arr = [];
+  for (let i = 0; i < words.length; i++) {
+    if (i === 0) arr.push(words[i]);
+    else if (
+      !arr.includes(words[i]) &&
+      words[i - 1][words[i - 1].length - 1] === words[i][0]
+    ) {
+      arr.push(words[i]);
+    } else {
+      return [(i % n) + 1, Math.floor(i / n) + 1];
+    }
+  }
+
+  return [0, 0];
+}
