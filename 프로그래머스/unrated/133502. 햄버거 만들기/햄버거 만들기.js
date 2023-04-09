41abacad21e148a75bfeab5afd8e086751368083
zhal7779 <79049046+zhal7779@users.noreply.github.com>
2023-04-09 22:31:21 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,12 @@
+function solution(ingredient) {
+    let answer = 0;
+    let stack = [];
+    for(let i = 0; i< ingredient.length; i++){
+        stack.push(ingredient[i]);
+         if (stack.slice(-4).join('') === '1231') {
+              stack.splice(-4);
+              answer++;
+            }
+    }
+    return answer;
+}
