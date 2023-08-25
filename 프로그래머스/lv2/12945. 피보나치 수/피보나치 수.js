ca2afdec23fc217e5c287d34174e7cb61e4040d8
minyoung <79049046+zhal7779@users.noreply.github.com>
2023-08-26 02:08:31 +0900

    [level 2] Title: 피보나치 수, Time: 1.72 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -4,11 +4,10 @@ function solution(n){
     let f1 = 1;
     //**i는 2부터 시작한다!! 피보나치 수 계산은 F(2)부터 시작되니깐!
     for(let i=2; i<=n; i++){
-        answer = 0;
-        answer += (f0 + f1)%1234567;
+        answer = (f0 + f1) % 1234567;
         f0 = f1;
         f1 = answer;
         
     }
-  return answer;
+  return answer ;
 }
